3b0970fec3ebdf595fa7a3e84a16a63c10b2bdd3
Nassim Ben Hassine <79005429+Nassimbhs@users.noreply.github.com>
2022-11-17 00:13:33 +0100

    Update SpringFoxSwaggerConfig.java

--- src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
+++ src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
@@ -19,7 +19,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
-@EnableSwagger2
 public class SpringFoxSwaggerConfig {
 
 	public static final String AUTHORIZATION_HEADER = "Authorization";
