090d6f803e67ae480364f08a971553c156eb4c1f
wannessnadia <nadia.wanness@esprit.tn>
2022-11-16 18:55:54 -1100

    Commit Mockito test

--- src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
@@ -1,21 +1,25 @@
 package tn.esprit.rh.achat;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
-import tn.esprit.rh.achat.services.IOperateurService;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
 
 @RunWith(SpringRunner.class)
@@ -23,24 +27,63 @@ import tn.esprit.rh.achat.services.OperateurServiceImpl;
 @ExtendWith(MockitoExtension.class)
 public class OperateurServiceImplTest {
 	
-	//@InjectMocks
-	//OperateurServiceImpl operateurService;
-	
-	@Autowired
-	IOperateurService operateurService;
-	
-	//@Autowired
+	@InjectMocks
+	OperateurServiceImpl operateurService;
 	@Mock
-	OperateurRepository operatuerRepo;
+	OperateurRepository operateurRepo;
 	
 	@Test
 	public void addOperateurTestMock() {
-		Operateur o = new Operateur(7L,"Ouanes","Nadia","123",null);
-		//when(operatuerRepo.save(o)).thenReturn(o);
-		assertEquals(o,operateurService.addOperateur(o));
+		
+	        Operateur o = new Operateur();
+	        o.setIdOperateur(9L);
+	        o.setNom("Ouannes");
+	        o.setPrenom("Nadia");
+	        o.setPassword("123");
+	        when(operateurRepo.save(ArgumentMatchers.any(Operateur.class))).thenReturn(o);
+	        assertSame(operateurService.addOperateur(o), o);
+	        verify(operateurRepo).save(o);
 	}
 	
 	@Test
+	public void retrieveAllOperateursTestMock() {
+
+		Operateur o1 = new Operateur(7L,"Ouanes","Nadia","123",null);
+		Operateur o2 = new Operateur(8L,"Ouanes","Syrine","123",null);
+        List<Operateur> listOperateurs = new ArrayList<Operateur>();
+        listOperateurs.add(o1);
+        listOperateurs.add(o2);
+        BDDMockito.given(operateurRepo.findAll()).willReturn(listOperateurs);
+        List<Operateur> expected = operateurService.retrieveAllOperateurs();
+        assertEquals(listOperateurs, expected);
+        verify(operateurRepo).findAll();
+
+    }
+	
+	@Test
+	public void deleteOperateurTestMock() {
+
+		Operateur o = new Operateur(8L,"Ouanes","Nadia","123",null);
+     
+        when(operateurRepo.findById(o.getIdOperateur())).thenReturn(Optional.of(o));
+        operateurService.deleteOperateur(o.getIdOperateur());
+        verify(operateurRepo).deleteById(o.getIdOperateur());
+
+    }
+	
+	@Test
+	public void retrieveOperateurTestMock() {
+
+		Operateur o = new Operateur(8L,"Ouanes","Nadia","123",null);
+		
+        when(operateurRepo.findById(o.getIdOperateur())).thenReturn(Optional.of(o));
+
+        assertSame(operateurService.retrieveOperateur(o.getIdOperateur()),o);
+        verify(operateurRepo).findById(o.getIdOperateur());
+
+    }
+	
+	/* @Test
 	public void addOperateurTest() {
 		
 			Operateur o = new Operateur(6L,"Ouanes","Nadia","123",null);
@@ -64,7 +107,7 @@ public class OperateurServiceImplTest {
 	public void deleteOperateurTest() {
 		//Stock s = new Stock("stock test",30,60);
 		//Stock savedStock= stockService.addStock(s);
-		operateurService.deleteOperateur(6L);
+		operateurService.deleteOperateur(7L);
 		//assertNull(stockService.retrieveStock(savedStock.getIdStock()));
-	} 
+	} */
 }
